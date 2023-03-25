fc1e893ff5148157557c05be5370766250693619
Francisco Lourenço <franciscolour2011@gmail.com>
2023-03-25 13:39:58 +0100

    fix resquest name

--- src/main/java/com/example/starwars/domain/PlanetController.java
+++ src/main/java/com/example/starwars/domain/PlanetController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/planetes")
+@RequestMapping("/planets")
 public class PlanetController {
 
     private PlanetService planetService;
