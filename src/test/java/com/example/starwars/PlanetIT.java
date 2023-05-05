dd9e8252ec565ae477329e2b43a7cd8c6afefc47
Francisc Lourenço <franciscolour2011@gmail.com>
2023-05-05 14:29:42 +0100

    doing integration test in Planet created

---
+++ src/test/java/com/example/starwars/PlanetIT.java
@@ -0,0 +1,36 @@
+package com.example.starwars;
+
+import static com.example.starwars.common.PlanetConstants.PLANET;
+
+import com.example.starwars.domain.Planet;
+import static  org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+@ActiveProfiles("it")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(scripts = "/remove_planets.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class PlanetIT {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @Test
+    public void createdPlanet_ReturnsCreated(){
+
+        ResponseEntity<Planet> sut = restTemplate.postForEntity("/planets", PLANET, Planet.class);
+
+        assertThat(sut.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(sut.getBody()).isNotNull();
+        assertThat(sut.getBody().getName()).isEqualTo(PLANET.getName());
+        assertThat(sut.getBody().getTerrain()).isEqualTo(PLANET.getTerrain());
+        assertThat(sut.getBody().getClimate()).isEqualTo(PLANET.getClimate());
+
+    }
+
+}
